8ed96522eaa30c35b176e0743746c9a7e4c9dd3e
juwonheur <133929384+juwon-heo@users.noreply.github.com>
2025-08-06 12:29:05 +0900

    Meta-Eyetracking first commit
    first commit

---
+++ Assets/Script/EyeGazeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeGazeController : MonoBehaviour
+{
+    public GameObject arrow;
+    OVREyeGaze eyeGaze;
+
+   
+    void Start()
+    {
+        eyeGaze = GetComponent<OVREyeGaze>();
+    }
+
+    
+    void Update()
+    {
+        if (eyeGaze == null) return;
+
+       
+        if (eyeGaze.EyeTrackingEnabled)
+        {
+           
+            arrow.transform.rotation = eyeGaze.transform.rotation;
+        }
+    }
+}
+
